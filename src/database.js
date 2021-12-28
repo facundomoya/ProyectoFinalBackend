d7280ecb34f9d362199efb25ca295261068e0044
GonzaloC <cainzog@gmail.com>
2021-12-28 02:32:12 -0300

    2

--- src/database.js
+++ src/database.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 //const url = "mongodb://127.0.0.1:27017/proyectofinalbackend"
-const url = "mongodb+srv://facundomoya18:navidad14@cluster0.1utx0.mongodb.net/proyectofinalbackend"
+const url = "mongodb+srv://gonzalo:gonzalo@cluster0.dkvnn.mongodb.net/proyectofinal16igrupo2";
 
 mongoose.connect(url)
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
  connection.once("open",()=>{
-    console.log("BD conectada")
+    console.log("BD conectada");
 }) 
